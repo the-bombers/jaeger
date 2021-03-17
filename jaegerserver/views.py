5795c1f09f85729faeccb11827520ea9f9eb174c
Ruaraidh <rca.plummer@gmail.com>
2021-03-17 16:58:42 +0000

    # end of day Push

--- jaegerserver/views.py
+++ jaegerserver/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Monster, Sighting
+
 
 from django.http import HttpResponse
-
 def index(request):
- return HttpResponse("Monsters from Beyond") 
+    monsters = Monster.objects.all()
+    mns = ", ".join([m.species for m in monsters])
+    return HttpResponse(mns) 
+
+def sightings(request):
+    sightings = Sighting.objects.all()
+    monsterSightings = [s.monster.species for s in sightings]
+    return HttpResponse(monsterSightings)
--- web/urls.py
+++ web/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("jaegerserver/", include("jaegerserver.urls")),
+    path("", include("jaegerserver.urls")),
     path('admin/', admin.site.urls),
 ]
