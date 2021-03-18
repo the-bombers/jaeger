b2289d808ac6024ed17b3a5768ee974f60b5620c
Adam Manley <adam.p.manley@gmail.com>
2021-03-18 10:49:07 +0000

    added locations to admin server

--- jaegerserver/admin.py
+++ jaegerserver/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from jaegerserver.models import Monster, Sighting
+from jaegerserver.models import Monster, Sighting, Location
 
 admin.site.register(Monster)
 admin.site.register(Sighting)
+admin.site.register(Location)
