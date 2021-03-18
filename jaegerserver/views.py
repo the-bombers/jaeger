765b04a1cf4163d83aae2bd701ca9f42628b2a27
Stephen Booth <stephenboothdev@gmail.com>
2021-03-18 14:12:14 +0000

    added missed updates

--- jaegerserver/views.py
+++ jaegerserver/views.py
@@ -4,23 +4,28 @@ from .models import Monster, Sighting
 
 from django.http import HttpResponse
 
+from .models import Monster, Sighting
 
 def index(request):
+    return HttpResponse("Monsters from Beyond")
+def monsters(request):
     monsters = Monster.objects.all()
-    mns = ", ".join([m.species for m in monsters])
-    return HttpResponse(mns)
-
-
+    return render(request, "jaegerserver/monster_list.html", monsters)
 def sightings(request):
     sightings = Sighting.objects.all()
     monsterSightings = [s.monster.species for s in sightings]
     return HttpResponse(monsterSightings)
-
 def monsters_detail(request, monster_id):
     monster = Monster.objects.get(pk=monster_id)
-    return render(request, "jaeger/monster_detail.html", monster)
+    return render(request, "jaegerserver/monster_detail.html", monster)
 
 def locations(request):
     locations = Locations.objects.all()
-    monsterLocation = [l.monster.sighting for l in locations]
-    return HttpResponse(monsterLocation)
+    location_list = [l.location for l in locations]
+    return HttpResponse(location_list)
+
+def monsterSightings(request, monsters_id):
+    monster = Monster.objects.get(pk=monsters_id)
+    # monsterId = monsters()
+    # return HttpResponse(“I’m still working”)
+    return HttpResponse(monster.id)
