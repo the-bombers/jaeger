40fa62e90f108492a51244021787fe71350fccc8
jordan <jthomasw88>
2021-03-18 12:02:25 +0000

    comma

--- jaegerserver/urls.py
+++ jaegerserver/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('sightings/', views.sightings, name="sightings"),
-    path('monsters/<int:monster_id>/', views.monsters_detail, name="monsters_details")
+    path('monsters/<int:monster_id>/',
+         views.monsters_detail, name="monsters_details"),
     path("monsters/", views.monsters, name="monsters"),
     path("locations/", views.locations, name="locations"),
 ]
