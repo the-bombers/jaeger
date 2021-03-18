3bdb925b4f23155e55dd1970a81039bf77cfc4c6
Ruaraidh <rca.plummer@gmail.com>
2021-03-18 17:24:28 +0000

    #12 monster sitings by day!

--- jaegerserver/models.py
+++ jaegerserver/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-
 class Monster(models.Model):
     species = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-
 
 class Sighting(models.Model):
     monster = models.ForeignKey(Monster, on_delete=models.CASCADE)
--- jaegerserver/views.py
+++ jaegerserver/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from .models import Monster, Sighting
-
+from .models import Monster, Sighting, Location
 
 from django.http import HttpResponse
 
@@ -8,24 +7,28 @@ from .models import Monster, Sighting
 
 def index(request):
     return HttpResponse("Monsters from Beyond")
+
 def monsters(request):
     monsters = Monster.objects.all()
     return render(request, "jaegerserver/monster_list.html", monsters)
+
 def sightings(request):
     sightings = Sighting.objects.all()
     monsterSightings = [s.monster.species for s in sightings]
     return HttpResponse(monsterSightings)
+
 def monsters_detail(request, monster_id):
     monster = Monster.objects.get(pk=monster_id)
     return render(request, "jaegerserver/monster_detail.html", monster)
 
 def locations(request):
-    locations = Locations.objects.all()
+    locations = Location.objects.all()
     location_list = [l.location for l in locations]
     return HttpResponse(location_list)
 
 def monsterSightings(request, monsters_id):
     monster = Monster.objects.get(pk=monsters_id)
-    # monsterId = monsters()
+    sightingsList = monster.sighting_set.all()
+    sightDay = [s.day for s in sightingsList]
+    return HttpResponse(sightDay)
     # return HttpResponse(“I’m still working”)
-    return HttpResponse(monster.id)
