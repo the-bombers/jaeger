1c5bd91a4337a44062565df7ca7eede3f4f06037
Jennifer <jennifacarey@gmail.com>
2021-03-18 10:29:41 +0000

    create monsters view with routing

--- jaegerserver/urls.py
+++ jaegerserver/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("monsters/", views.monsters, name="monsters"),
     path('sightings/', views.sightings, name="sightings")
 ]
