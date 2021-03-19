2e229192c17c77cc6b82f59aa3aadd48a71b3e43
Adam Manley <adam.p.manley@gmail.com>
2021-03-19 10:36:10 +0000

    views Fixed

--- jaegerserver/views.py
+++ jaegerserver/views.py
@@ -1,35 +1,29 @@
 from django.shortcuts import render
 from .models import Monster, Sighting, Location
-
-
 from django.http import HttpResponse
 
-from .models import Monster, Sighting
 
 def index(request):
     return HttpResponse("Monsters from Beyond")
 def monsters(request):
-    monsters = Monster.objects.all()
-    return render(request, "jaegerserver/monster_list.html", monsters)
+    context = {"monsters": Monster.objects.all()}
+    return render(request, "jaegerserver/monster_list.html", context)
 def sightings(request):
     sightings = Sighting.objects.all()
     monsterSightings = [s.monster.species for s in sightings]
     return HttpResponse(monsterSightings)
 def monsters_detail(request, monster_id):
-    context= {"monster": Monster.objects.get(pk=monster_id)}
-    return render(request, "jaegerserver/monster_detail.html", context)
-
+    monster = Monster.objects.get(pk=monster_id)
+    return render(request, "jaegerserver/monster_detail.html", monster)
 def locations(request):
-    locations = Location.objects.all()
-    location_list = [l.location for l in locations]
-    return HttpResponse(location_list)
-
-def locations_detail(request, location_id):
-    context = {"location": Location.objects.get(pk=location_id)}
-    return render(request, "jaegerserver/location_detail.html", context)
-
+    context = {"locations": Location.objects.all()}
+    return render(request, "jaegerserver/locations.html", context)
 def monsterSightings(request, monsters_id):
     monster = Monster.objects.get(pk=monsters_id)
-    # monsterId = monsters()
+    sightingsList = monster.sighting_set.all()
+    sightDay = [s.day for s in sightingsList]
+    return HttpResponse(sightDay)
     # return HttpResponse(“I’m still working”)
-    return HttpResponse(monster.id)
+
+
+
