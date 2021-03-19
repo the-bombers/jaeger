86d7e3c44d4f74f731ac47f03b9c34438d279999
Adam Manley <adam.p.manley@gmail.com>
2021-03-19 11:47:25 +0000

    Locations added to db monsters added db

--- jaegerserver/models.py
+++ jaegerserver/models.py
@@ -4,7 +4,7 @@ class Monster(models.Model):
     species = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     danger_level = models.IntegerField(default=0)
-    features = models.JSONField()
+    features = models.JSONField(default=["REDACTED"])
 
 class Sighting(models.Model):
     monster = models.ForeignKey(Monster, on_delete=models.CASCADE)
@@ -13,4 +13,4 @@ class Sighting(models.Model):
 class Location(models.Model):
     sighting = models.ForeignKey(Sighting, on_delete=models.CASCADE)
     location = models.CharField(max_length=50)
-    history = models.CharField(max_length=1000)
+    history = models.CharField(max_length=1000, default="GAREASDFASDAFASDFF")
