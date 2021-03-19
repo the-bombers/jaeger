77b899894f0e0893445acb8211f3c386f1eacedc
jordan <jthomasw88>
2021-03-19 10:07:10 +0000

    fixing list of locations
    list fix

--- jaegerserver/views.py
+++ jaegerserver/views.py
@@ -9,23 +9,29 @@ from .models import Monster, Sighting, Location
 def index(request):
     return HttpResponse("Monsters from Beyond")
 
+
 def monsters(request):
     context = {"monsters": Monster.objects.all()}
     return render(request, "jaegerserver/monster_list.html", context)
+
 
 def sightings(request):
     sightings = Sighting.objects.all()
     monsterSightings = [s.monster.species for s in sightings]
     return HttpResponse(monsterSightings)
 
+
 def monsters_detail(request, monster_id):
     monster = Monster.objects.get(pk=monster_id)
     return render(request, "jaegerserver/monster_detail.html", monster)
 
+
 def locations(request):
-    locations = Location.objects.all()
-    location_list = [l.location for l in locations]
-    return HttpResponse(location_list)
+
+    context = {"locations": Location.objects.all()}
+
+    return render(request, "jaegerserver/locations.html", context)
+
 
 def monsterSightings(request, monsters_id):
     monster = Monster.objects.get(pk=monsters_id)
@@ -33,7 +39,3 @@ def monsterSightings(request, monsters_id):
     sightDay = [s.day for s in sightingsList]
     return HttpResponse(sightDay)
     # return HttpResponse(“I’m still working”)
-
-    context = {"locations": Location.objects.all()}
-    # location_list = [l.location for l in locations]
-    return render(request, "jaegerserver/locations.html", context)
