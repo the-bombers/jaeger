949ec40727c77e8c75830e238b26007fd428c4ae
Stephen Booth <stephenboothdev@gmail.com>
2021-03-19 14:58:51 +0000

    Added data

--- jaegerserver/models.py
+++ jaegerserver/models.py
@@ -5,12 +5,16 @@ class Monster(models.Model):
     name = models.CharField(max_length=200)
     danger_level = models.IntegerField(default=0)
     features = models.JSONField(default=["REDACTED"])
+    about = models.CharField(max_length=2000)
     def __str__(self):
         return self.name
 
 class Sighting(models.Model):
     monster = models.ForeignKey(Monster, on_delete=models.CASCADE)
-    day = models.CharField(max_length=20)
+    date = models.DateTimeField(max_length=20)
+    description = models.CharField(max_length=2000)
+    day= models.CharField(max_length=20)
+
     def __str__(self):
         return f"{self.monster.name}: {self.day}"
 
